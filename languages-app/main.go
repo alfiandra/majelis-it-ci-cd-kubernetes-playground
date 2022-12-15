cd56259b02cabfb157245f6e7667e469d8e14a6e
alfiankan <alfiankan19@gmail.com>
2022-12-15 18:52:53 +0700

    Update main.go

--- languages-app/main.go
+++ languages-app/main.go
@@ -92,7 +92,7 @@ func main() {
 	app.Get("/v1/languages", prometheus.Middleware, GetAvailableLanguages)
 
 	app.Get("/health", func(c *fiber.Ctx) error {
-		return c.Status(200).SendString("VERSION DIUBAH")
+		return c.Status(200).SendString(APP_VERSION)
 	})
 
 	log.Fatal(app.Listen(APP_DEFAULT_PORT))
